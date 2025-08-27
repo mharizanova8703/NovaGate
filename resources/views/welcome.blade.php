6d0fd826a078213ac09d73bbcdfa9d0dbf191d73
Mariya Harizanova <mharizanova@yahoo.com>
2025-08-27 09:13:01 -0400

    favicon update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
-
+<link rel="icon" href="{{ asset('images/favicon.ico') }}" type="image/x-icon">
     <!-- Bootstrap Icons -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 
